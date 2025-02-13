235776e85c899a27c9e1050437fd7d1ec724d70a
Jiri Trinh <trnh.anhtu@gmail.com>
2025-02-13 18:37:27 +0100

    create app Users for registration and set up database

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 
 posts = [
     {
@@ -21,7 +22,7 @@ posts = [
 # Create your views here.
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
