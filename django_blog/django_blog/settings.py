f90873a133c389950a016623351ee77201f64396
Jiri Trinh <trnh.anhtu@gmail.com>
2025-04-23 18:09:51 +0200

    setting up User profile and Picture

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 
 # Default primary key field type
@@ -135,3 +139,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
